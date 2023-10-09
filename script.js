2d8fe3ca991c724d82561af2c52a17f2fbb2b561
Rafael S.P <71520261+Rafa-py@users.noreply.github.com>
2023-10-08 22:58:43 -0300

    delete the script.js

--- script.js
+++ script.js
@@ -5,38 +5,6 @@
 // [X] Mostrar o resultado para o usuário
 
 let app = ()=>{
-    let form = document.querySelector(".form");
-    
-    form.addEventListener("submit", (event)=>{
-        event.preventDefault();
-
-        getData()
-    })
-
-    let getData = ()=>{
-        let base = (document.querySelector("#base")).value;
-        let altura = (document.querySelector("#altura")).value;
-        if(base == "" || altura == ""){
-            alert("Preencha todos os dados para calcular")
-        }else{
-            if(isNaN(parseInt(base)) || isNaN(parseInt(altura))){
-                alert("Digite um número para calcular")
-            }else{
-                calc(base, altura)
-            }
-        }
-    }
-
-    let calc = (a, b)=>{
-        let result = (a * b) / 2
-        showResultToUser(result);
-    }
-
-    let showResultToUser = (value)=>{
-        let p = document.querySelector("#result");
-
-        p.innerHTML = value
-    }
 }
 
 app();
